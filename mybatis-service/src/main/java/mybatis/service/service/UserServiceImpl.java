aabf6e0f994d4a55a847ba766bc0b851248f61cb
smoner <quanzhongmeng@126.com>
2017-02-02 23:53:50 +0800

    spring+mybatis+maven  + oracle  实现了单条数据的增删改查和批量的增删改查

--- mybatis-service/src/main/java/mybatis/service/service/UserServiceImpl.java
+++ mybatis-service/src/main/java/mybatis/service/service/UserServiceImpl.java
@@ -5,6 +5,9 @@ import mybatis.service.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by smoner on 2017/1/31.
  */
@@ -15,5 +18,47 @@ public class UserServiceImpl implements IUserService {
 
     public User selectUserById(Integer userId) {
         return userDao.selectUserById(userId);
+    }
+
+    public User selectUserByName(String userName) {
+        return userDao.selectUserByName(userName);
+    }
+
+    public void updateUserById(User user) {
+        userDao.updateUserById(user);
+    }
+
+    public Integer insertUser(User user) {
+        Integer integer =  userDao.insertUser(user);
+        return integer ;
+    }
+
+    public Integer deleteUserById(Integer integer) throws Throwable {
+        try{
+            userDao.deleteUserById(integer);
+        }catch (Exception e){
+            throw new Throwable(e.getMessage());
+        }
+        return integer;
+    }
+
+    public Integer batchInsertUser(List<User> users) {
+        return userDao.batchInsertUser(users);
+    }
+
+    public Integer batchUpdateUser(List<User> users) {
+        return userDao.batchUpdateUser(users);
+    }
+
+    public Integer batchDeleteUser(List<User> users) {
+        return userDao.batchDeleteUser(users);
+    }
+
+    public List<User> batchQueryUser(List<User> users) {
+        return userDao.batchQueryUser(users);
+    }
+
+    public List<HashMap> batchQueryUserMap(List<User> users) {
+        return userDao.batchQueryUserMap(users);
     }
 }
